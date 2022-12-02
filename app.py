93a65580addc1310d3141d7f4ae837299a5b884d
Ajeng Nilta Adriani <114748439+Ajengnilta@users.noreply.github.com>
2022-12-02 15:43:52 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import pickle
+
+# App Initialization
+app = Flask(__name__)
+
+# Load the Model
+pipeline = pickle.load(open('model_milestones1.pkl','rb'))
+model = tf.keras.models.load_model('model_churn.h5')
+
+# Endpoint for Homepage
+@app.route("/")
+def home():
+    return "<h1>It Works!</h1>"
+
+# Endpoint for Prediction
+@app.route("/predict", methods=['POST'])
+def model_predict():
+    args = request.json
+    new_data = {'tenure': args.get('tenure'),
+    'MonthlyCharges': args.get('MonthlyCharges'),
+    'SeniorCitizen': args.get('SeniorCitizen'),
+    'Partner': args.get('Partner'),
+    'Dependents': args.get('Dependents'),
+    'InternetService': args.get('InternetService'),
+    'DeviceProtection': args.get('DeviceProtection'),
+    'StreamingTV': args.get('StreamingTV'),
+    'StreamingMovies': args.get('StreamingMovies'),
+    'Contract': args.get('Contract'),
+    'PaperlessBilling': args.get('PaperlessBilling'),
+    'PaymentMethod': args.get('PaymentMethod')}
+
+    new_data = pd.DataFrame([new_data])
+    print('New Data : ', new_data)
+
+    #pipeline
+    X = pipeline.transform(new_data)
+
+    # Predict
+    y_label = ['No','Yes']
+    y_pred = int(np.round(model.predict(X)[0][0]))
+
+    # Return the Response
+    response = jsonify(
+      result = str(y_pred), 
+      label_names = y_label[y_pred])
+
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
